c6a485fedbd587739d4feabed0d12742dc30c356
DANIELCOREI7\Daniel <daniel238035@hotmail.com>
2018-09-15 02:03:53 -0500

    Primer commit Index

---
+++ frontend/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+    <script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+    <link rel="stylesheet" href="css/estilos.css">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+
+
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="cd-stretchy-nav add-content nav-inferior" style="background: none; box-shadow: none;">
+            <a class="cd-nav-trigger" href="#0">
+                Menu
+                <span aria-hidden="true"></span>
+            </a>
+
+            <ul>
+                <li><a href="#0"><span>Farmacia</span></a></li>
+                <li><a href="#0"><span>Consulta</span></a></li>
+                <li><a href="#0"><span>Nosotros</span></a></li>
+                <li><a href="#0"><span>Cuenta</span></a></li>
+            </ul>
+
+            <span aria-hidden="true" class="stretchy-nav-bg"></span>
+        </nav>
+    </header>
+
+    <main class="cd-main-content portada-principal" >
+        <!-- main content here -->
+        <div class="container ">
+            <div class="row">
+                <nav style="background: none; box-shadow: none;">
+                    <div class="nav-wrapper">
+                        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+                            <i class="material-icons">menu</i>
+                        </a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li >
+                                <a class="cl-plomo negrita" href="sass.html">Personas</a>
+                            </li>
+                            <li >
+                                <a class="cl-plomo negrita" href="badges.html">Empresas</a>
+                            </li>
+                        </ul>
+
+                        <ul class="sidenav" id="mobile-demo">
+                            <li>
+                                <a class="cl-azul negrita" href="sass.html">Personas</a>
+                            </li>
+                            <li>
+                                <a class="cl-azul negrita" href="badges.html">Empresas</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="row  ">
+                <div class="col s8 l6 offset-l3">
+                    <img class="responsive-img" src="img/logo.png" >
+                </div>
+            </div>
+            <div class="row">
+                <div class="container">
+                    <div class="row" style="padding: 30px;">
+                        <div class="col s12 l3 center-align">
+                            <img class="responsive-img" src="img/icono1.png" style="width: 80%;">
+                        </div>
+                        <div class="col s12 l3 center-align">
+                            <img class="responsive-img" src="img/icono2.png" style="width: 80%;">
+                        </div>
+                        <div class="col s12 l3 center-align">
+                            <img class="responsive-img" src="img/icono3.png" style="width: 80%;">
+                        </div>
+                        <div class="col s12 l3 center-align">
+                            <img class="responsive-img" src="img/icono4.png" style="width: 80%;">
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </main>
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+    <script src="js/main.js"></script>
+    <!-- Resource jQuery -->
+
+    <script src="js/scrip.js"></script>
+</body>
+
+</html>
